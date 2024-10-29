85375b1cbc8fc38a15d6f52839a562e8164400a0
Yushin-Yoshinao <133735696+Yushin-Yoshinao@users.noreply.github.com>
2024-10-29 11:03:57 +0900

    section3-3
    空中ジャンプしてしまう

--- section3-3/sketch.js
+++ section3-3/sketch.js
@@ -1,32 +1,47 @@
-// テキスト「キーボード操作に反応する」
-let x, y;
+let x, y, vx, vy;
+const g = 1;
+const vyMax = 20;
 
-function setup(){
+function setup() {
   createCanvas(windowWidth, windowHeight);
   x = width / 2;
   y = height / 2;
+  vx = random(-2, 2); // 修正: randon → random
+  vy = 0;
 }
 
-function draw(){
+function draw() {
   background(160, 192, 255);
   ellipse(x, y, 50);
-  if(keyIsDown(LEFT_ARROW)){ x -= 5; }
-  if(keyIsDown(RIGHT_ARROW)){ x += 5; }
-  if(keyIsDown(UP_ARROW)){ y -= 5; }
-  if(keyIsDown(DOWN_ARROW)){ y += 5; }
-  if(keyIsDown("A".charCodeAt(0))){ x+= 10; }
-  if(keyIsDown(" ".charCodeAt(0))){ x-= 10; }
+  x += vx;
+  y += vy;
+  vy += g;
+  vy = constrain(vy, -vyMax, vyMax);
+  
+  if (x < 0 || x > width) {
+    vx = -1 * vx;
+  }
+  if (y < 0 || y > height) {
+    vy = -1 * vy;
+  }
+  y = constrain(y, 0, height);
+
+  // キー操作
+  if (keyIsDown(LEFT_ARROW)) { x -= 5; }
+  if (keyIsDown(RIGHT_ARROW)) { x += 5; }
+  if (keyIsDown(UP_ARROW)) { y -= 5; }
+  if (keyIsDown(DOWN_ARROW)) { y += 5; }
+  if (keyIsDown("A".charCodeAt(0))) { x += 10; }
+  if (keyIsDown(" ".charCodeAt(0))) { x -= 10; }
 }
 
-// イベントハンドラを使用するパターン
-// function keyPressed(){
-//   if(keyCode == LEFT_ARROW){ x -= 5; }
-//   else if(keyCode == RIGHT_ARROW){ x+= 5; }
-//   else if(keyCode == DOWN_ARROW){ y += 5; }
-//   else if(keyCode == UP_ARROW){ y -= 5; }
-//   else if(key == "A"){ x += 10; }
-// }
+function keyPressed() {
+  if (key === ' ') { // スペースキーが押されたとき
+    vy = -15; // ジャンプのための上向きの速度を設定
+  }
+}
 
-function windowResized(){
+function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
+
