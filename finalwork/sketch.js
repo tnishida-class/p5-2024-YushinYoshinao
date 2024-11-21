c4cd6f0ed6f9add244271d3b4c63a46b070d6428
Yushin-Yoshinao <133735696+Yushin-Yoshinao@users.noreply.github.com>
2024-11-21 10:42:35 +0900

    シフトを押すと目の色が変わるミャクミャク

--- finalwork/sketch.js
+++ finalwork/sketch.js
@@ -1,13 +1,85 @@
-// 最終課題を制作しよう
-
-function setup(){
-  createCanvas(windowWidth, windowHeight);
+function setup() {
+  createCanvas(400, 400);
+  background(20);
 }
 
-function draw(){
-  background(160, 192, 255);
+function draw() {
+  drawFace();
+  drawMouth();
+  drawEyes(keyIsDown(16)); // シフトキーが押されているかを渡す
 }
 
-function windowResized(){
-  resizeCanvas(windowWidth, windowHeight);
+// 顔を描画
+function drawFace() {
+  fill(65, 105, 255); // 青色
+  ellipse(200, 200, 200, 150); // 顔
+}
+
+// 口を描画
+function drawMouth() {
+  stroke(65, 105, 255);
+  
+  // 下の口（左側を少し上げる）
+  fill(255); // 白色
+  arc(200, 175, 80, 130, QUARTER_PI / 6, PI);
+  
+  // 上の口（反対向き）
+  fill(65, 105, 255); // 青色
+  arc(200, 175, 80, 40, 0.125, PI);
+}
+
+// 目を描画
+function drawEyes(isShiftPressed) {
+  noStroke();
+
+  // 赤い目（背景の大きな部分）
+  fill(255, 0, 0); // 赤色
+  let redEyes = [
+    [220, 125, 65, 90],  // 本体の右目
+    [160, 130, 65, 65],  // 本体の左目
+    [210, 280, 60, 50],  // 口の真下
+    [160, 280, 65, 75],  // 左下
+    [110, 240, 65, 65],  // 左上
+    [100, 190, 45, 45],  // 小さい左目
+    [120, 160, 50, 35],  // 左目の手前
+    [250, 270, 35, 70],  // 右下
+    [290, 240, 65, 65],  // 右上
+    [290, 185, 55, 55],  // 右の中
+    [260, 150, 60, 45]   // 小さい右目
+  ];
+  for (let i = 0; i < redEyes.length; i++) {
+    ellipse(...redEyes[i]);
+  }
+
+  // 白目と青目（配列データを使用）
+  let whiteEyes = [
+    [222, 100, 28, 28],
+    [155, 140, 30, 30],
+    [300, 235, 30, 30],
+    [96, 235, 25, 25],
+    [160, 293, 35, 35]
+  ];
+  let blueEyes = [
+    [225, 94, 14, 14],
+    [161, 138, 15, 15],
+    [305, 239, 15, 15],
+    [92, 237, 12, 12],
+    [165, 298, 17, 17]
+  ];
+
+  // 白目を描画
+  fill(255); // 白色
+  for (let i = 0; i < whiteEyes.length; i++) {
+    ellipse(...whiteEyes[i]);
+  }
+
+  // 青目または赤目を描画
+  if (isShiftPressed) {
+    fill(255, 0, 0); // 赤色（シフトキーが押されている場合）
+  } else {
+    fill(65, 105, 255); // 青色（通常時）
+  }
+  for (let i = 0; i < blueEyes.length; i++) {
+    ellipse(...blueEyes[i]);
+  }
 }
--- section5-3/sketch.js
+++ section5-3/sketch.js
@@ -1,63 +1,56 @@
-// テキスト「関数を作る(2) 結果を戻す関数」～「総仕上げ：カレンダーを描画しよう」
-function setup(){
+function setup() {
+
   createCanvas(200, 200);
-  calendar(2019, 10);
 
-  // isLeapYear の動作確認のため console に出力しています
-  for(let i = 2000; i <= 2100; i++){
-    if(isLeapYear(i)){
-      console.log(i + "年はうるう年です");
-    }
-    else{
-      console.log(i + "年はうるう年ではありません");
+  // isLeapYear関数を使って、うるう年かどうかをコンソールに出力
+  for (let i = 2000; i <= 2100; i++) {
+    if (isLeapYear(i)) {
+      console.log(i + "年はうるう年です"); 
+    } else {
+      console.log(i + "年はうるう年ではありません"); 
     }
   }
 }
 
-function calendar(y, m){
-  let dow = dayOfWeek(y, m, 1);
-  for(let d = 1; d <= daysInMonth(y, m); d++){
-    // BLANK[3] (hint: まずは daysInYear, dayOfWeek を作ろう)
-  }
-}
-
-function isLeapYear(y){
+// うるう年かどうかを判定する関数
+function isLeapYear(y) {
+  // 4で割り切れ、かつ100で割り切れない または 400で割り切れる年がうるう年である。
   return (y % 4 == 0) && (y % 100 != 0) || (y % 400 == 0);
 }
 
-function daysInYear(y){
-  function getDaysInYear(y) {
-    if ((y % 4 === 0 && y % 100 !== 0) || y % 400 === 0) {
-      return 366;  // うるう年
-    } else {
-      return 365;  // 普通の年
-    }
-  }
-  
+// 1年の日数を返す関数
+function daysInYear(y) {
+  // うるう年なら366日、それ以外は365日
+  return isLeapYear(y) ? 366 : 365;
 }
 
-function daysInMonth(y, m){
-  if(m == 2){
+// 指定した月の日数を返す関数
+function daysInMonth(y, m) {
+  if (m == 2) {
+    // 2月はうるう年かどうかで29日または28日
     return isLeapYear(y) ? 29 : 28;
-  }
-  else if(m == 4 || m == 6 || m == 9 || m == 11){
+  } else if (m == 4 || m == 6 || m == 9 || m == 11) {
+    // 4月、6月、9月、11月は30日
     return 30;
-  }
-  else{
+  } else {
+    // その他の月は31日
     return 31;
   }
 }
 
-function dayOfYear(y, m, d){
-  let count = 0;
-  for(let i = 1; i < m; i++){
+// 年、月、日を指定して、その年の何日目かを返す関数
+function dayOfYear(y, m, d) {
+  let count = 0; // 日数の合計を記録する変数
+  for (let i = 1; i < m; i++) {
+    // 1月から指定した月の1つ前までの日数を加算
     count += daysInMonth(y, i);
   }
+  // 指定した日の分を加算
   return count + d;
 }
 
-function dayOfWeek(y, m, d){
-  // BLANK[2]
+// 年、月、日を指定して、その曜日を計算する関数
+function dayOfWeek(y, m, d) {
   const daysInMonth = [
     31, // 1月
     28, // 2月
@@ -72,31 +65,30 @@ function dayOfWeek(y, m, d){
     30, // 11月
     31  // 12月
   ];
-  
-  // 1970年1月1日からy年m月d日までの日数を計算
-  let totalDays = 0;
-  
-  // 1年目からy-1年目までの日数
+
+  let totalDays = 0; // 1970年1月1日からの累計日数を計算するための変数
+
+  // 1970年から指定した年の前年までの日数を加算
   for (let year = 1970; year < y; year++) {
     totalDays += isLeapYear(year) ? 366 : 365;
   }
-  
-  // 1月からm-1月までの日数
+
+  // 1月から指定した月の1つ前までの日数を加算
   for (let month = 1; month < m; month++) {
     totalDays += (month === 2 && isLeapYear(y)) ? 29 : daysInMonth[month - 1];
   }
-  
-  // m月のd日までの日数
-  totalDays += d - 1;  // 1日からカウントするのでd-1
-  
-  // 基準日1970年1月1日が木曜日(4)で、その日を0とする
-  // その経過日数を7で割った余りを求める
-  let dayOfWeek = (totalDays + 4) % 7;
-  
-  return dayOfWeek;
+
+  // 指定した月の日数を加算
+  totalDays += d - 1; // 1日目からカウントなので -1
+
+  // 1970年1月1日(木曜日)を基準に曜日を計算
+  let dayOfWeek = (totalDays + 4) % 7; // 4は木曜日のオフセット
+
+  return dayOfWeek; // 曜日を表す数値 (0: 日曜日, 1: 月曜日, ..., 6: 土曜日)
 }
 
-function dayOfWeekAsString(dow){
-  const a = ["日", "月", "火", "水", "木", "金", "土", "日"];
-  return a[dow];
+// 曜日を表す数値を対応する文字列に変換する関数
+function dayOfWeekAsString(dow) {
+  const a = ["日", "月", "火", "水", "木", "金", "土"]; // 曜日配列
+  return a[dow]; // 数値を使って曜日文字を返す
 }
